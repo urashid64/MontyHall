77a1911e2eacbeb7880490240382d109c51802fc
Usman Rashid <usman.rashid@gmail.com>
2019-01-25 16:57:26 -0800

    Initial Commit

---
+++ MontyHall/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MontyHall
+//
+//  Created by Usman Rashid on 1/25/19.
+//  Copyright © 2019 Usman Rashid. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
